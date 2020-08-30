3824cdbf9774653dfbff80e7e482a3d14f939577
Vera Dias <veradias97@hotmail.com>
2020-08-30 22:28:37 +0100

    Updates

---
+++ precache-manifest.e74a01a0aaa4a5b14cdc072782a11427.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b0d73e2e8c1a81427fe6579394d2b6a",
+    "url": "/Monsters-Web-App-React-Redux/index.html"
+  },
+  {
+    "revision": "3e9f87b396df093130d1",
+    "url": "/Monsters-Web-App-React-Redux/static/css/main.a5dcdfdf.chunk.css"
+  },
+  {
+    "revision": "ddfc98fd1fb7cc1baeab",
+    "url": "/Monsters-Web-App-React-Redux/static/js/2.fcb60162.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monsters-Web-App-React-Redux/static/js/2.fcb60162.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e9f87b396df093130d1",
+    "url": "/Monsters-Web-App-React-Redux/static/js/main.4f277ec1.chunk.js"
+  },
+  {
+    "revision": "e0ebf62f8cbf91b4da4a",
+    "url": "/Monsters-Web-App-React-Redux/static/js/runtime-main.1ed0e272.js"
+  }
+]);
